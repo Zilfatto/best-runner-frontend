385984f9dbb5162a8c5d1f737cb863078ed91bb2
dkrasnenko <krasnenko.daniil@list.ru>
2021-05-05 15:11:55 +0700

    Add a service layer for api logic and react toastify for showing notifications

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
     return (
-        <main></main>
+        <React.Fragment>
+            <ToastContainer />
+        </React.Fragment>
     );
 }
 
---
+++ src/services/httpService.ts
@@ -0,0 +1,31 @@
+import axios, { AxiosError } from 'axios';
+import { toast } from 'react-toastify';
+
+axios.interceptors.response.use(response => response, httpErrorHandler);
+
+// Handle unexpected errors
+function httpErrorHandler(error: AxiosError) {
+    // Determine if an error is unexpected
+    const expectedError =
+        error.response &&
+        error.response.status >= 400 &&
+        error.response.status < 500;
+
+    // Log an unexpected error to a Raven imitation and show an error to a user
+    if (!expectedError) {
+        console.error(error);
+        toast.error('An unexpected error occurred.');
+    }
+
+    return Promise.reject(error);
+}
+
+// HTTP methods for easy using
+const httpMethods = {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete
+};
+
+export default httpMethods;
